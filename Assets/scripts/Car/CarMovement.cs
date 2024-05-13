159d63a099d769164f545ee44b3ef36ffbc76591
OctaviusThe1st <161426545+OctaviusThe1st@users.noreply.github.com>
2024-05-13 18:15:15 +0300

    araba animasyon tamamlandı

--- Assets/scripts/Car/CarMovement.cs
+++ Assets/scripts/Car/CarMovement.cs
@@ -7,6 +7,7 @@ public class CarMovement : MonoBehaviour
     [SerializeField] private DialogueSystem DialogueSystem;
     private bool coroutineRunning = false;
     private Coroutine coroutine;
+    public Animator car_drift;
     void Start()
     {
         if (DialogueSystem == null)
@@ -14,44 +15,13 @@ public class CarMovement : MonoBehaviour
             DialogueSystem.GetComponent<DialogueSystem>();
         }
     }
-
-    void Update()
+    private void Update()
     {
-        if(!DialogueSystem.isDialogue)
-        transform.Translate(new Vector3(0, 0.1f, 0));
-
-
-        if(!DialogueSystem.isDialogue && !coroutineRunning)
+        if (!DialogueSystem.isDialogue)
         {
-            coroutine = StartCoroutine(CarMoveOrder());
-            coroutineRunning = true;
+            car_drift.SetBool("CarAnimBool", true);
         }
-        else if (DialogueSystem.isDialogue && coroutineRunning)
-        {
-            StopCoroutine(coroutine);
-            coroutineRunning = false;
-        }
-
-    }
-
-    IEnumerator CarMoveOrder()
-    {
-        yield return new WaitForSeconds(1.3f);
-
-        Quaternion startRotation = Quaternion.Euler(-90, 0, -90);
-        Quaternion endRotation = Quaternion.Euler(-90, 90, -90);
-
-        float duration = 5.0f;
-        float elapsed = 0.0f;
-
-        while (elapsed < duration)
-        {
-            float t = elapsed / duration; 
-            transform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
-            elapsed += Time.deltaTime; 
-            yield return null; 
-        }
-
-        transform.rotation = endRotation;
+        else { return; }
     }
 }
+
